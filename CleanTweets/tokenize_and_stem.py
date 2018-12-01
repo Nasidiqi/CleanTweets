572914ac7581bb822e2b6363d80c37deb2fc13dc
Jessica Fogerty <jessicafogerty@Jessicas-MacBook-Pro.local>
2018-12-01 14:27:31 -0500

    Remove print debug statements

--- CleanTweets/tokenize_and_stem.py
+++ CleanTweets/tokenize_and_stem.py
@@ -1,11 +1,9 @@
 def tokenizeandstem(df):
     """This function tokenizes the tweets and stems them and then joins them back together."""
     tokenized_tweet = df['tidy_tweet'].apply(lambda x: x.split())
-    print('*****')
     stemmer = PorterStemmer()
     tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x])
     for i in range(len(tokenized_tweet)):
         tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
-    print('*****')
     df['tidy_tweet'] = tokenized_tweet
     return df
