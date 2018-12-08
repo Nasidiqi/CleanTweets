9bb548d07a4df78524bd7a5c4aa163ab3bc6f837
Jessica Fogerty <jessicafogerty@Jessicas-MacBook-Pro.local>
2018-12-08 13:13:13 -0500

    Edit load data

--- CleanTweets/LoadData.py
+++ CleanTweets/LoadData.py
@@ -1,7 +1,4 @@
-def load_data('data'):
-    Df= pd.read_excel('Data')
-    return(Df)
+def load_data():
+    df= pd.read_excel('https://sfm.library.gwu.edu/ui/exports/755/file/14a72258a1b441a8a89385a6c1a064ad_001.xlsx')
 
-def view_head(Df):
-    x = Df.head()
-    return(x)
+return(df)
