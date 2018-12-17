c34a18b9d476592b591ee8e91a3809035c855b43
Jessica Fogerty <jessicafogerty@Jessicas-MacBook-Pro.local>
2018-12-17 14:41:32 -0800

    delete line 7 in main.py. Made relative path to data directory

--- CleanTweets/load_data.py
+++ CleanTweets/load_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 def load_data():
-    os.chdir('Data')
-    df = pd.read_excel('10.23-10.24.xlsx')
+    df = pd.read_excel('Data/10.23-10.24.xlsx')
     return df
-    os.chdir('..')
+
+load_data()
